3e8820723c315183836a4b9cfbd3fa5a73f9458a
Runar Kristoffersen <public@rkmedia.no>
2017-11-13 10:02:42 +0100

    Move defaultData

--- api-server/categories.js
+++ api-server/categories.js
@@ -1,31 +1,14 @@
 const clone = require('clone')
 const config = require('./config')
-
+const defaultData = require('./defaultData')
 let db = {}
-
-const defaultData = {
-  categories: [
-      {
-        name: 'react',
-        path: 'react'
-      },
-      {
-        name: 'redux',
-        path: 'redux'
-      },
-      {
-        name: 'udacity',
-        path: 'udacity'
-      }
-  ]
-}
 
 function getData (token) {
   //Each token has it's own copy of the DB. The token in this case is like an app id.
   let data = db[token]
   //This populates the default user data if there isn't any in the db.
   if (data == null) {
-    data = db[token] = clone(defaultData)
+    data = db[token] = clone(defaultData.categories)
   }
   return data
 }
